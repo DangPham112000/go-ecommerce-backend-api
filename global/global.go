ef48e02f810116bc98fdc768f317b1473857c6f5
DangPham112000 <dangpham112000@gmail.com>
2024-10-08 11:20:05 +0700

    init Redis

--- global/global.go
+++ global/global.go
@@ -3,6 +3,7 @@ package global
 import (
 	"github.com/DangPham112000/go-ecommerce-backend-api/pkg/logger"
 	"github.com/DangPham112000/go-ecommerce-backend-api/pkg/setting"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
@@ -10,4 +11,5 @@ var (
 	Config setting.Config
 	Logger *logger.LoggerZap
 	Mdb    *gorm.DB
+	Rbd    *redis.Client
 )
--- internal/initialize/redis.go
+++ internal/initialize/redis.go
@@ -1,5 +1,33 @@
 package initialize
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/DangPham112000/go-ecommerce-backend-api/global"
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
 func InitRedis() {
+	r := global.Config.Redis
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%v", r.Host, r.Port), // "localhost:6379",
+		Password: r.Password,                           // no password set
+		DB:       r.Database,                           // use default DB
+		PoolSize: 10,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+
+	if err != nil {
+		global.Logger.Error("Redis initialization error")
+	}
+
+	fmt.Println("Redis is running")
+
+	global.Rbd = rdb
 
 }
