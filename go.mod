40608a49e42a8a2e964bfa7176fe5ab3c77bef83
DangPham112000 <dangpham112000@gmail.com>
2024-10-08 11:00:45 +0700

    mysql init

--- go.mod
+++ go.mod
@@ -15,7 +15,9 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.1 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -52,5 +54,6 @@ require (
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 )
--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,5 +1,54 @@
 package initialize
 
-func InitMysql() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/DangPham112000/go-ecommerce-backend-api/global"
+	"github.com/DangPham112000/go-ecommerce-backend-api/internal/po"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func checkErrorPanic(err error, errStr string) {
+	if err != nil {
+		global.Logger.Error(errStr, zap.Error(err))
+		panic(err)
+	}
+}
+
+func InitMysql() {
+	m := global.Config.Mysql
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	s := fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+	checkErrorPanic(err, "Mysql initialization error")
+	global.Logger.Info("Mysql initialization success!!!")
+	global.Mdb = db
+
+	SetPool()
+	migrateTables()
+}
+
+func SetPool() {
+	m := global.Config.Mysql
+	sqlDb, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Printf("Mysql error :: %s", err)
+	}
+	sqlDb.SetMaxIdleConns(m.MaxIdleConns)
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime))
+}
+
+func migrateTables() {
+	err := global.Mdb.AutoMigrate(&po.User{}, &po.Role{})
+	if err != nil {
+		fmt.Println("Tables Migration error")
+	}
 }
